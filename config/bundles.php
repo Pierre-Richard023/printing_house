5baf2f55240c8cf87d0fee60d6fa598ccad93de1
Pierre-Richard BEBE <pierre.richard.prb@gmail.com>
2024-09-01 18:52:46 -0300

    maj symfony 5.4 to 6.4

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
